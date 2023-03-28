5ca3a001cd51842d50106d2df34a701aeb2b13b2
hwone9 <hwone9@gmail.com>
2023-03-28 21:27:40 +0900

    ui수정 및 styled-component, react-icons 설치

--- project/src/App.js
+++ project/src/App.js
@@ -1,39 +1,50 @@
-import { useEffect, useState } from "react";
-import TodoComponent from "./components/TodoComponent";
+// import { useEffect, useState } from "react";
+// import TodoComponent from "./components/TodoComponent";
+
+import { createGlobalStyle } from "styled-components";
+import TodoTemplate from "./components/TodoTemplate";
+
+const GlobalStyle = createGlobalStyle`
+  body{
+    background : #f4f4f4;
+  }
+`;
 
 function App() {
-  const [todo, setTodo] = useState("");
-  const [todoList, setTodoList] = useState([]);
+  // const [todo, setTodo] = useState("");
+  // const [todoList, setTodoList] = useState([]);
 
-  const onChange = (event) => {
-    setTodo(event.target.value);
-  };
-  const onSubmit = (event) => {
-    event.preventDefault();
-    if (todo === "") {
-      return;
-    }
-    setTodoList((currentArray) => [
-      { id: todoList.length + 1, todo: todo },
-      ...currentArray,
-    ]);
-    setTodo("");
-  };
-  //init
-  useEffect(() => {
-    fetch(`${window.location.href}data/getTodoList.json`)
-      .then((response) => response.json())
-      .then((json) => {
-        setTodoList(json);
-      });
-  }, []);
+  // const onChange = (event) => {
+  //   setTodo(event.target.value);
+  // };
+  // const onSubmit = (event) => {
+  //   event.preventDefault();
+  //   if (todo === "") {
+  //     return;
+  //   }
+  //   setTodoList((currentArray) => [
+  //     { id: todoList.length + 1, todo: todo },
+  //     ...currentArray,
+  //   ]);
+  //   setTodo("");
+  // };
+  // //init
+  // useEffect(() => {
+  //   fetch(`${window.location.href}data/getTodoList.json`)
+  //     .then((response) => response.json())
+  //     .then((json) => {
+  //       setTodoList(json);
+  //     });
+  // }, []);
 
-  console.log(todo);
-  console.log(todoList);
+  // console.log(todo);
+  // console.log(todoList);
 
   return (
     <div>
-      <h1>TO DO LIST</h1>
+      <GlobalStyle />
+      <TodoTemplate></TodoTemplate>
+      {/* <h1>TO DO LIST</h1>
       <form onSubmit={onSubmit}>
         <input
           value={todo}
@@ -45,7 +56,7 @@ function App() {
       {todoList.map((item) => (
         //<li key={item.id}> {item.todo} </li>
         <TodoComponent key={item.id} id={item.id} todo={item.todo} />
-      ))}
+      ))} */}
     </div>
   );
 }
